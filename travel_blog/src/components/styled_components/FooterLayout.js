5cd9bc406b4c63e057b89a1aa74c7be747fa67c4
JustJeckill1993 <evgeniyduplenko@gmail.com>
2020-07-05 21:17:47 +0300

    main template

--- travel_blog/src/components/styled_components/FooterLayout.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import styled from 'styled-components';
-import HeaderLayout from "./HeaderLayout";
-
-const FooterLayout = styled(HeaderLayout)`
-    
-`
-
-export default FooterLayout;
